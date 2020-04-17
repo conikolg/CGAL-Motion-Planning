70e8a38292b09724553e1cdf43e3341c2cc25c61
Nikhil Gupta <nxg160630@utdallas.edu>
2020-04-16 21:01:06 -0500

    compiles a CGAL example program

--- CMakeFiles/main.o.dir/DependInfo.cmake
+++ CMakeFiles/main.o.dir/DependInfo.cmake
@@ -4,12 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/nikhilgupta/Files/College/Spring20/CS6301/Project/main.cpp" "/Users/nikhilgupta/Files/College/Spring20/CS6301/Project/cmake-build-debug/CMakeFiles/Project.dir/main.cpp.o"
+  "/Users/nikhilgupta/Files/College/Spring20/CS6301/Project/main.cpp" "/Users/nikhilgupta/Files/College/Spring20/CS6301/Project/CMakeFiles/main.o.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/Cellar/cgal/5.0.2/include"
   )
 
 # Targets to which this target links.
