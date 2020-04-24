ea4b93d5ffdf4ce1bf1a5f10817a6f539eb984d7
Nikhil Gupta <nxg160630@utdallas.edu>
2020-04-24 16:06:14 -0500

    sample code for tmap

--- main.cpp
+++ main.cpp
@@ -1,117 +1,116 @@
-// Author(s): Camille Wormser, Pierre Alliez
-// An example of an AABB tree constructed with custom point and triangle types.
-
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
 #include <iostream>
-#include <list>
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/AABB_tree.h>
-#include <CGAL/AABB_traits.h>
+#include <CGAL/Arr_polycurve_traits_2.h>
+#include <CGAL/Arrangement_2.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Quotient.h>
+#include <CGAL/Arr_trapezoid_ric_point_location.h>
+#include <CGAL/Arr_vertical_decomposition_2.h>
+#include <CGAL/Object.h>
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point;
+typedef CGAL::Polygon_2<K> Polygon_2;
 
-typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Quotient<CGAL::MP_Float> Number_type;
+typedef CGAL::Cartesian<Number_type> Kernel;
+typedef CGAL::Arr_segment_traits_2<Kernel> Traits_2;
+typedef Traits_2::Point_2 Point_2;
+typedef Traits_2::X_monotone_curve_2 Segment_2;
+typedef CGAL::Arrangement_2<Traits_2> Arrangement_2;
+typedef CGAL::Arr_trapezoid_ric_point_location<Arrangement_2> Trap_pl;
 
-
-// custom point type
-struct My_point {
-    double m_x;
-    double m_y;
-    double m_z;
-
-    My_point(const double x,
-             const double y,
-             const double z)
-            : m_x(x), m_y(y), m_z(z) {}
-};
-
-// custom triangle type with
-// three pointers to points
-struct My_triangle {
-    My_point *m_pa;
-    My_point *m_pb;
-    My_point *m_pc;
-
-    My_triangle(My_point *pa,
-                My_point *pb,
-                My_point *pc)
-            : m_pa(pa), m_pb(pb), m_pc(pc) {}
-};
-
-// the custom triangles are stored into a vector
-typedef std::vector<My_triangle>::const_iterator Iterator;
-
-// The following primitive provides the conversion facilities between
-// the custom triangle and point types and the CGAL ones
-struct My_triangle_primitive {
-public:
-
-    // this is the type of data that the queries returns. For this example
-    // we imagine that, for some reasons, we do not want to store the iterators
-    // of the vector, but raw pointers. This is to show that the Id type
-    // does not have to be the same as the one of the input parameter of the
-    // constructor.
-    typedef const My_triangle *Id;
-
-    // CGAL types returned
-    typedef K::Point_3 Point; // CGAL 3D point type
-    typedef K::Triangle_3 Datum; // CGAL 3D triangle type
-
-private:
-    Id m_pt; // this is what the AABB tree stores internally
-
-public:
-    My_triangle_primitive() {} // default constructor needed
-
-    // the following constructor is the one that receives the iterators from the
-    // iterator range given as input to the AABB_tree
-    My_triangle_primitive(Iterator it)
-            : m_pt(&(*it)) {}
-
-    const Id &id() const { return m_pt; }
-
-    // utility function to convert a custom
-    // point type to CGAL point type.
-    Point convert(const My_point *p) const {
-        return Point(p->m_x, p->m_y, p->m_z);
+void print_neighboring_vertices(Arrangement_2::Vertex_const_handle v) {
+    if (v->is_isolated()) {
+        std::cout << "The vertex (" << v->point() << ") is isolated" << std::endl;
+        return;
     }
-
-    // on the fly conversion from the internal data to the CGAL types
-    Datum datum() const {
-        return Datum(convert(m_pt->m_pa),
-                     convert(m_pt->m_pb),
-                     convert(m_pt->m_pc));
-    }
-
-    // returns a reference point which must be on the primitive
-    Point reference_point() const { return convert(m_pt->m_pa); }
-};
-
-
-typedef CGAL::AABB_traits<K, My_triangle_primitive> My_AABB_traits;
-typedef CGAL::AABB_tree<My_AABB_traits> Tree;
+    Arrangement_2::Halfedge_around_vertex_const_circulator first, curr;
+    first = curr = v->incident_halfedges();
+    std::cout << "The neighbors of the vertex (" << v->point() << ") are:";
+    do {
+        // Note that the current halfedge is directed from u to v:
+        Arrangement_2::Vertex_const_handle u = curr->source();
+        std::cout << " (" << u->point() << ")";
+    } while (++curr != first);
+    std::cout << std::endl;
+}
 
 int main() {
-    My_point a(1.0, 0.0, 0.0);
-    My_point b(0.0, 1.0, 0.0);
-    My_point c(0.0, 0.0, 1.0);
-    My_point d(0.0, 0.0, 0.0);
+    // Defines a polygon
+//    Point points[] = {Point(0, 0), Point(5.1, 0), Point(1, 1), Point(0.5, 6)};
+//    Polygon_2 pgn(points, points + 4);
+    // check if the polygon is simple.
+//    std::cout << "The polygon is " <<
+//         (pgn.is_simple() ? "" : "not ") << "simple." << std::endl;
+//    // check if the polygon is convex
+//    std::cout << "The polygon is " <<
+//         (pgn.is_convex() ? "" : "not ") << "convex." << std::endl;
 
-    std::vector<My_triangle> triangles;
-    triangles.emplace_back(&a, &b, &c);
-    triangles.emplace_back(&a, &b, &d);
-    triangles.emplace_back(&a, &d, &c);
+    // An arrangement
+    Arrangement_2 arr;
+    // Define points of a polygon
+    Point_2 points2[] = {Point_2(0, 0), Point_2(5, 0), Point_2(5, 5), Point_2(0, 5)};
+    // Number of points
+    int num = sizeof(points2) / sizeof(points2[0]);
+    // Array of segments
+    Segment_2 curve[num];
+    // Generate segments
+    for (int i = 0; i < num - 1; i++)
+        curve[i] = Segment_2(points2[i], points2[i + 1]);
+    // Generate the final cycling segment
+    curve[num - 1] = Segment_2(points2[num - 1], points2[0]);
+    // Insert the segments
+    CGAL::insert(arr, &curve[0], &curve[num]);
 
-    // constructs AABB tree
-    Tree tree(triangles.begin(), triangles.end());
+    print_neighboring_vertices(arr.vertices_begin());
 
-    // counts #intersections
-    K::Ray_3 ray_query(K::Point_3(1.0, 0.0, 0.0), K::Point_3(0.0, 1.0, 0.0));
-    std::cout << tree.number_of_intersected_primitives(ray_query)
-              << " intersections(s) with ray query" << std::endl;
+    Trap_pl trap_pl;
+    trap_pl.attach(arr);
 
-    // computes closest point
-    K::Point_3 point_query(2.0, 2.0, 2.0);
-    K::Point_3 closest_point = tree.closest_point(point_query);
-    std::cerr << "closest point is: " << closest_point << std::endl;
+    typedef Arrangement_2::Vertex_const_handle Vertex_const_handle;
+    typedef std::pair<Vertex_const_handle, std::pair<CGAL::Object, CGAL::Object>> Vert_decomp_entry;
+    typedef std::list<Vert_decomp_entry> Vert_decomp_list;
+    Vert_decomp_list vd_list;
+    CGAL::decompose(arr, std::back_inserter(vd_list));
 
-    return EXIT_SUCCESS;
+    typedef Arrangement_2::Halfedge_const_handle Halfedge_const_handle;
+    typedef Arrangement_2::Face_const_handle Face_const_handle;
+    // Print the results.
+    Vert_decomp_list::const_iterator vd_iter;
+    std::pair<CGAL::Object, CGAL::Object> curr;
+    Vertex_const_handle vh;
+    Halfedge_const_handle hh;
+    Face_const_handle fh;
+
+    for (vd_iter = vd_list.begin(); vd_iter != vd_list.end(); ++vd_iter) {
+        curr = vd_iter->second;
+        std::cout << "Vertex (" << vd_iter->first->point() << ") : ";
+
+        std::cout << " feature below: ";
+        if (CGAL::assign(vh, curr.first))
+            std::cout << '(' << vh->point() << ')';
+        else if (CGAL::assign(fh, curr.first))
+            if (!fh->is_fictitious())
+                std::cout << '[' << fh->outer_ccb().ptr() << ']';
+            else
+                std::cout << "NONE";
+        else
+            std::cout << "EMPTY";
+
+        std::cout << "   feature above: ";
+        if (CGAL::assign(vh, curr.second))
+            std::cout << '(' << vh->point() << ')' << std::endl;
+        else if (CGAL::assign(hh, curr.second))
+            if (!fh->is_fictitious())
+                std::cout << '[' << fh->outer_ccb().ptr() << ']' << std::endl;
+            else
+                std::cout << "NONE" << std::endl;
+        else
+            std::cout << "EMPTY" << std::endl;
+    }
+
+    return 0;
 }
